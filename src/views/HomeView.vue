3821dca50fa0956fc6830868bff3c8d91f0299a8
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2025-04-03 02:31:37 -0300

    feat: cria o projeto, e desenvolve a página principal

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// Components
+import OperadorasTable from '@/components/OperadorasComponents/OperadorasTable.vue';
+</script>
+
+<template>
+	<main>
+		<OperadorasTable />
+	</main>
+</template>
+
+<style scoped>
+main {
+	position: relative;
+	display: flex;
+	flex-wrap: wrap;
+	width: 100%;
+	height: fit-content;
+	justify-self: center;
+}
+</style>
